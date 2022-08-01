63e52c0eef7945bdf662cff9a6ff11333eaef39b
Lauren Mizner <lmizner@hotmail.com>
2022-08-01 15:28:40 -0700

    challenge 3: find the first k missing positive numbers

--- grokking_cyclic_sort.py
+++ grokking_cyclic_sort.py
@@ -282,4 +282,54 @@ main()
 
 ################################################################################################
 
+# CHALLENGE 3: FIND THE FIRST K MISSING POSITIVE NUMBERS
+# Given an unsorted array containing numbers and a number 'k', find the first 'k' missing positive
+# numbers in the array.
+
+def find_first_k_missing_positive(nums, k):
+    n = len(nums)
+    i = 0
+
+    while i < len(nums):
+        j = nums[i] - 1
+
+        if nums[i] > 0 and nums[i] <= n and nums[i] != nums[j]:
+            # swap locations in the array
+            nums[i], nums[j] = nums[j], nums[i]
+        # move to the next step in the cycle
+        else:
+            i += 1
+
+    missing_numbers = []
+    extra_numbers = set()
+
+    for i in range(n):
+        if len(missing_numbers) < k:
+            if nums[i] != i + 1:
+                missing_numbers.append(i+1)
+                extra_numbers.add(nums[i])
+
+    # add the remaining missing numbers
+    i = 1
+
+    while len(missing_numbers) < k:
+        candidate_number = i + n
+
+        # ignore if the array contains the candidate number
+        if candidate_number not in extra_numbers:
+            missing_numbers.append(candidate_number)
+        i += 1
+
+    return missing_numbers
+
+
+def main():
+    print(find_first_k_missing_positive([3, -1, 4, 5, 5], 3))
+    print(find_first_k_missing_positive([2, 3, 4], 3))
+    print(find_first_k_missing_positive([-2, -3, 4], 2))
+
+main()
+
+# Time Complexity - O(n) and O(k) respectively
+# Space Complexity - O(k)
 
