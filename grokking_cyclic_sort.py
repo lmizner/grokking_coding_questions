b6e87c1fb05820067f7e8d61ba63e44424296f64
Lauren Mizner <lmizner@hotmail.com>
2022-08-01 12:03:25 -0700

    Find all missing numbers

--- grokking_cyclic_sort.py
+++ grokking_cyclic_sort.py
@@ -80,5 +80,42 @@ main()
 
 ################################################################################################
         
+# FIND ALL MISSING NUMBERS
+# Given an unsorted array containing numbers taken from the range 1 to 'n'. The array can have 
+# duplicates, which means some numbers will be missing. Find all those missing numbers.
+
+def find_missing_numbers(nums):
+    i = 0
+
+    while i < len(nums):
+        j = nums[i] - 1
+
+        # If the values are different...
+        if nums[i] != nums[j]:
+            # ...Swap locations in the array...
+            nums[i], nums[j] = nums[j], nums[i]
+        # ...until the array indices are the same, then move to the next i in the cycle
+        else:
+            i += 1
+    
+    missing_numbers = []
+
+    for i in range(len(nums)):
+        if nums[i] != i + 1:
+            missing_numbers.append(i + 1)
+
+    return missing_numbers
 
 
+def main():
+    print(find_missing_numbers([2, 3, 1, 8, 2, 3, 5, 1]))
+    print(find_missing_numbers([2, 4, 1, 2]))
+    print(find_missing_numbers([2, 3, 2, 1]))
+
+main()
+
+# Time Complexity - O(n)
+# Space Complexity - O(1)
+
+
+################################################################################################
